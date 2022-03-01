ed1f5475b5633936a46604a53457cb36b6ca8850
Shin Cong Knee <knee1997@gmail.com>
2022-03-01 23:38:32 +0800

    #00006 - Create ToDo Rest API controller

---
+++ src/main/java/com/todo/todo/controller/ToDoController.java
@@ -0,0 +1,50 @@
+package com.todo.todo.controller;
+import com.todo.todo.model.ToDo;
+import com.todo.todo.services.ToDoServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:8081")
+@RestController
+@RequestMapping("/api")
+
+public class ToDoController {
+    @Autowired
+    private ToDoServices toDoServices;
+
+    //POST
+    @PostMapping("/addToDo")
+    public ToDo addToDo(@RequestBody ToDo toDo) {
+        return toDoServices.addToDo(toDo);
+    }
+
+    //GET
+    @GetMapping("/toDo")
+    public List<ToDo> getAllToDo() {
+        return toDoServices.getToDo();
+    }
+    @GetMapping("/toDoById/{id}")
+    public ToDo findToDoById(@PathVariable int id) {
+        return toDoServices.getToDoById(id);
+    }
+    @GetMapping("/toDoByTitle/{title}")
+    public ToDo findToDoByTitle(@PathVariable String title) {
+        return toDoServices.getToDoByTitle(title);
+    }
+
+    //PUT
+    @PutMapping("/update/{id}")
+    public ToDo updateToDo(@RequestBody ToDo toDo, @PathVariable int id)
+    {
+        System.out.println("UPDATED");
+        return toDoServices.updateToDo(toDo);
+    }
+
+    //DELETE
+    @DeleteMapping("/delete/{id}")
+    public String deleteToDo(@PathVariable int id) {
+        return toDoServices.deleteToDo(id);
+    }
+}
